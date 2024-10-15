34b7d3834cd9df72512c449a92eaa11eb237e5e9
dwight-zhang <1776212922@qq.com>
2024-10-15 08:31:31 +0800

    modify displacement script with chen, cui, liu : we have one more step :)

--- Stress/Yuyang/09_26/displacement.py
+++ Stress/Yuyang/09_26/displacement.py
@@ -8,56 +8,65 @@ import numpy as np
 from scipy.integrate import quad
 
 #---------------- meshing ------------------
+R = 1
+pz_down = 0.0
+cz_down = 0.75
 geo = CSGeometry()
-sph = Sphere(Pnt(0, 0, 0.2), 1).bc("sph")
-down = Plane(Pnt(0, 0, 0), Vec(0, 0, -1)).bc("down")
-top = Plane(Pnt(0, 0, 1.1), Vec(0, 0, -1)).bc("top")
-sphtop = sph * top
-sphdown = sph * down
-pt = sphdown - sphtop 
-geo.AddSurface(sph, pt)
+sph1 = Sphere(Pnt(0, 0, 0), R).bc("pz")
+sph2 = Sphere(Pnt(0, 0, 0), R).bc("cz")
+down = Plane(Pnt(0, 0, pz_down), Vec(0, 0, -1)).bc("down")
+top = Plane(Pnt(0, 0, cz_down), Vec(0, 0, -1)).bc("top")
+sph1down = sph1 * down
+sph2top = sph2 * top
+pt = sph1down - sph2top 
+geo.AddSurface(sph1, pt)
 geo.AddSurface(down, pt)
-geo.AddSurface(top, pt)
-ngmesh = geo.GenerateMesh(maxh=0.08)
+geo.AddSurface(sph2, sph2top)
+ngmesh = geo.GenerateMesh(maxh=0.05)
 mesh = Mesh(ngmesh)
 mesh.Curve(2)
 Draw(mesh)
+input("Mesh Done!")
 
 #---------------- parameters ------------------
 Pi = np.pi
-orderset = 2
-dimset = mesh.dim
 v = 0.5
-Yg = 1
-Pout = 1
+pzofcz = 0.3
+Yg_cz = 1e-1
+Pout = 0.1
+Yg = CoefficientFunction([ pzofcz * Yg_cz if bc == "pz" else Yg_cz for bc in mesh.GetBoundaries()])
 
 #---------------- FESpace ------------------      
-fes = H1(mesh, order=orderset,  dirichlet="down|top", dim=dimset)
+orderset = 2
+dimset = mesh.dim
+fes = H1(mesh, order=orderset,  dirichlet="down", dim=dimset)
 u = fes.TrialFunction()
-nsurf = -specialcf.normal(dimset)
+nsurf = specialcf.normal(dimset)
 
 #-------------------- energy function --------------------------
-a = BilinearForm(fes, symmetric=False, check_unused=False)
+# penalty = 1e4
 u_grad = grad(u).Trace()
-Eu = 1/2*(u_grad.trans * u_grad - Id(dimset))
-Cg = CoefficientFunction(((Yg,v*Yg,0),(Yg,v*Yg,0),(0,0,Yg*(v-1)/2)),dims=(dimset,dimset))
+Eu = 1/2*(u_grad.trans +  u_grad)
+Cg = CoefficientFunction(((Yg,v*Yg,0),(v*Yg,Yg,0),(0,0,Yg*(1-v)/2)),dims=(dimset,dimset))
 Phi = Eu.trans * (Cg * Eu)
-a += Variation(Trace(Phi).Compile()*ds)
-ngradu = u_grad.trans * nsurf
-beta = Pout*nsurf
-a += Variation((ngradu-beta)*(ngradu-beta)*ds(definedon=fes.mesh.Boundaries("sph")))
+ngradu = u_grad * nsurf
+beta = Pout * nsurf
+Force = CoefficientFunction((0,0,1))
+a = BilinearForm(fes, symmetric=False, check_unused=False)
+a += Variation(Trace(Phi).Compile()*ds(definedon=fes.mesh.Boundaries("pz|cz")))
+a += Variation((InnerProduct(- Pout * nsurf,u))*ds(definedon=fes.mesh.Boundaries("pz|cz")))
 
 #------------------- grid functions ---------------------
 u = GridFunction(fes)
 du = GridFunction(fes)
 res = u.vec.CreateVector()
-ini = CoefficientFunction([sin(x*x*y*y)*z*(1.1-z)*nsurf])
-u.Set(ini,definedon=fes.mesh.Boundaries("sph"))
+ini = CoefficientFunction(Pout*(z-pz_down)*nsurf)
+u.Set(ini,definedon=fes.mesh.Boundaries("pz|cz"))
 Draw(u, mesh, 'displacement')
-input("initial value setted!")
+input("Initiate Done!")
 
 #------------------- damped Newton ---------------------
-step = 0.2
+step = 1
 for i in range(50):
    print ("Damped Newton iteration", i+1)
    print ("  energy = ", a.Energy(u.vec))
@@ -72,3 +81,9 @@ for i in range(50):
    if abs(r) < 1e-6:
        break
 
+
+u_grad = grad(u).Trace()
+Eu = 1/2*(u_grad.trans * u_grad - Id(dimset))
+print(u_grad)
+Draw( Eu, mesh, 'strain')
+input("Done!")
